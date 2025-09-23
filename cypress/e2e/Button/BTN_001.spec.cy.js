c9cb17713c187b0326d7a578f9ee84370b139327
Zahra <zacamp@proton.me>
2025-09-23 16:13:58 -0400

    cnarios BTN_001 test

---
+++ cypress/e2e/Button/BTN_001.spec.cy.js
@@ -0,0 +1,7 @@
+describe('Click Follow Button', () => {
+    it('Clicks Follow Button on TYS tab', () => {
+        cy.visit('https://www.cnarios.com/concepts/button#try-it-yourself')
+        cy.get(':nth-child(4) > .MuiPaper-root > .MuiCardContent-root > [aria-label="Follow"] > .MuiButtonBase-root').click()
+        cy.get('[aria-label="Unfollow"] > .MuiButtonBase-root').should('contain', 'Following')
+    })      
+})
