ca7afa10f5cd3fce969cd26f26156b57167816af
Zahra <zacamp@proton.me>
2025-09-25 12:52:52 -0400

    BTN_001 complete

--- cypress/e2e/Button/BTN_001.spec.cy.js
+++ cypress/e2e/Button/BTN_001.spec.cy.js
@@ -3,5 +3,7 @@ describe('Click Follow Button', () => {
         cy.visit('https://www.cnarios.com/concepts/button#try-it-yourself')
         cy.get(':nth-child(4) > .MuiPaper-root > .MuiCardContent-root > [aria-label="Follow"] > .MuiButtonBase-root').click()
         cy.get('[aria-label="Unfollow"] > .MuiButtonBase-root').should('contain', 'Following')
+        //cy.get('MuiButton-icon MuiButton-startIcon MuiButton-iconSizeMedium css-cveorv').should('be.visible')
+        cy.get('[aria-label="Unfollow"] > .MuiButtonBase-root > .MuiButton-icon > .MuiSvgIcon-root > path').should('be.visible')
     })      
 })
