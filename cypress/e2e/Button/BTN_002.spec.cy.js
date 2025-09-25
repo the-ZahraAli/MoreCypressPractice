ed2d4e8ab3fbc2172a14262fc123e320aeec3c89
Zahra <zacamp@proton.me>
2025-09-25 12:35:47 -0400

    BTN_002 complete

--- cypress/e2e/Button/BTN_002.spec.cy.js
+++ cypress/e2e/Button/BTN_002.spec.cy.js
@@ -1,14 +1,20 @@
-describe('Hover over follow botton', () => {
-    it('Hovers over Follow Button on TYS tab', () => {
+describe('Tool-tip visible while hovering over follow/unfollow botton', () => {
+    beforeEach(() => {
         cy.visit('https://www.cnarios.com/concepts/button#try-it-yourself')
-cy.get(':nth-child(2) > .MuiPaper-root > .MuiCardContent-root > [aria-label="Follow"] > .MuiButtonBase-root')
+    })
+    it('Shows "Follow" tool-tip', () => {
+        cy.get(':nth-child(2) > .MuiPaper-root > .MuiCardContent-root > [aria-label="Follow"] > .MuiButtonBase-root')
             .trigger('mouseover')
-    cy.get('.MuiTooltip-tooltip').should('be.visible').and('have.text', 'Follow')
+        cy.get('.MuiTooltip-tooltip').should('be.visible').and('have.text', 'Follow')
                 
-    
     })
 
-
+it('Shows "Unfollow" tool-tip', () => { 
+    cy.get(':nth-child(2) > .MuiPaper-root > .MuiCardContent-root > [aria-label="Follow"] > .MuiButtonBase-root').click()
+    cy.get('[aria-label="Unfollow"] > .MuiButtonBase-root')
+            .realHover()
+    cy.get('.MuiTooltip-tooltip').should('be.visible').and('have.text', 'Unfollow')
+})
 
 
 
